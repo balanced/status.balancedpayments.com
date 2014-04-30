0f23b69e1e152aabd0d33fd4e3cb756ed41ceb6c
Victor Lin <bornstub@gmail.com>
2014-04-30 13:56:28 -0700

    Try to fix import issue

--- situation/tweepy/__init__.py
+++ situation/tweepy/__init__.py
@@ -9,13 +9,13 @@ __version__ = '2.1'
 __author__ = 'Joshua Roesslein'
 __license__ = 'MIT'
 
-from situation.tweepy.models import Status, User, DirectMessage, Friendship, SavedSearch, SearchResults, ModelFactory, Category
-from situation.tweepy.error import TweepError
-from situation.tweepy.api import API
-from situation.tweepy.cache import Cache, MemoryCache, FileCache
-from situation.tweepy.auth import OAuthHandler
-from situation.tweepy.streaming import Stream, StreamListener
-from situation.tweepy.cursor import Cursor
+from .tweepy.models import Status, User, DirectMessage, Friendship, SavedSearch, SearchResults, ModelFactory, Category
+from .tweepy.error import TweepError
+from .tweepy.api import API
+from .tweepy.cache import Cache, MemoryCache, FileCache
+from .tweepy.auth import OAuthHandler
+from .tweepy.streaming import Stream, StreamListener
+from .tweepy.cursor import Cursor
 
 # Global, unauthenticated instance of API
 api = API()
